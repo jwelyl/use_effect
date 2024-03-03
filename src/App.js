fa7997072459cfffcec58cf8ac7fb636ad071ed2
jwelyl <cork2586@naver.com>
2024-03-03 22:29:14 +0900

    [feat] useEffect로 최초 렌더링 시에만 특정 메서드 실행

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter((current) => current + 1);
 
+	//  렌더링때마다 출력될 로그
+  console.log("I run all the time");
+
   //  최초 렌더링 시 API 호출
   useEffect(() => {
     console.log("CALL THE API...");
